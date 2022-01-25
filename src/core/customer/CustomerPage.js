99150ccce661931e15c6b2bf55339b1eb8f6d4a2
intiravs <intira.vinit@gmail.com>
2022-01-25 11:29:32 +0700

    TEST

--- src/core/customer/CustomerPage.js
+++ src/core/customer/CustomerPage.js
@@ -6,6 +6,7 @@ const CustomerPage = () => {
         <Base title="Customer" desc="Vel-Suede Customer System">
             <div>
                 <h1>Customer Page</h1>
+                <h2>TEST</h2>
             </div>
         </Base>
     )
